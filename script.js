fe7fd2aa2894e05e26c2d3cd0bcf9d4e689e56ec
A1exeySvirid0v <92254547+A1exeySvirid0v@users.noreply.github.com>
2022-12-25 18:14:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+let firstValue = 15;
+let secondValue = 16;
+
+console.log(firstValue + " and " + secondValue);
+
+let sum = firstValue + secondValue;
+console.log(sum);
+
+let str = "JaVaScriPT";
+let upperCase = str.toUpperCase();
+console.log("Upper Case - " + upperCase);
+let lowerCase = str.toLowerCase();
+console.log("Lower Case - " + lowerCase);
+
+let summa = firstValue + secondValue;
+let raznost = secondValue - firstValue;
+let umnoj = firstValue * secondValue;
+let delen = secondValue / firstValue;
+let kvad1 = Math.pow(firstValue, 2);
+let kvad2 = Math.pow(secondValue, 2);
+let ost = secondValue % firstValue;
+
+console.log("Сложенение: " + summa);
+console.log("Вычитание: " + raznost);
+console.log("Умножение: " + umnoj);
+console.log("Деление: " + delen);
+console.log("Остаток от деления: " + ost);
+console.log("Возведение в квадрат 1-ого: " + kvad1);
+console.log("Возведение в квадрат 2-ого: " + kvad2);
+
+
+
+
